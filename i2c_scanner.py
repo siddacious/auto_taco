d725f8453388528a1c2694a865767fd6843e43e6
siddacious <nospam187+github@gmail.com>
2020-06-03 13:45:05 -0700

    added simple i2c scanner for pygamer advance

---
+++ i2c_scanner.py
@@ -0,0 +1,45 @@
+import time
+import terminalio
+import displayio
+import board
+from adafruit_display_text import label
+
+display = board.DISPLAY
+i2c = board.I2C()
+
+Y_OFFSET=30
+
+display = board.DISPLAY
+font = terminalio.FONT
+t_group = displayio.Group()
+while True:
+    while not i2c.try_lock():
+        time.sleep(0.010)
+    while len(t_group) > 0:
+        t_group.pop()
+    things = i2c.scan()
+
+    if len(things) == 0:
+        print("No I2C Devices responded to scan")
+        # Create the test label
+        not_found = label.Label(terminalio.FONT, text="No I2C Devices \nresponded to scan", color=0xFFFFFF, scale=2)
+        not_found.x = 75
+        not_found.y = Y_OFFSET+80
+        t_group.append(not_found)
+
+    else:
+        found_str = ""
+        for index, thing in enumerate(things):
+            found_str +="Found %d (%s)\n"%(thing, hex(thing))
+        found = label.Label(terminalio.FONT, text=found_str, color=0xFFFFFF, scale=2)
+        found.x = 75
+        found.y = Y_OFFSET+20
+        t_group.append(found)
+        print(found_str)
+    i2c.unlock()
+    display.show(t_group)
+
+    print("*************** Done! *************")
+
+    time.sleep(1)
+
